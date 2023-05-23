c27c49f61adf0ab2c619ef0831c37b3141a2ab27
minhkieu702 <115796407+minhkieu702@users.noreply.github.com>
2023-05-23 09:41:53 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ components/header/Header.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import TopHeader from "./TopHeader";
+import BottomHeader from "./BottomHeader";
+import styles from './Header.module.css'; 
+
+const Header = () => {
+  return (
+    <header className="header_wrap">
+      <TopHeader />
+      <div className="middle-header dark_skin">
+        <div className="container">
+          <div className="nav_block">
+            <a className="navbar-brand" href="index.html">
+              
+              <div className={styles.flex}>
+              <img
+                className= {styles.logo}
+                src="assets/images/image-141@2x.png"
+                alt="logo"
+              />
+                   <h2 style={{ whiteSpace: 'nowrap', textOverflow: 'ellipsis', fontFamily: 'Pacifico', fontSize: '36px', fontWeight: 400, lineHeight: '34px',  textAlign: 'left' }}>Tiệm Homie</h2>
+                   </div>
+            </a>
+            <div className="contact_phone order-md-last">
+              <i className="linearicons-phone-wave" />
+              <span>093 839 30 84</span>
+            </div>
+            <div className="product_search_form">
+              <form>
+                <div className="input-group">
+                  <div className="input-group-prepend">
+                    <div className={`${styles.custom_select} custom_select`}>
+                      <select className="first_null">
+                        <option value="">Categories</option>
+                        <option value="Dresses">Dresses</option>
+                        <option value="Shirt-Tops">Shirt &amp; Tops</option>
+                        <option value="T-Shirt">T-Shirt</option>
+                        <option value="Pents">Pents</option>
+                        <option value="Jeans">Jeans</option>
+                      </select>
+                    </div>
+                  </div>
+                  <input
+                    className="form-control"
+                    placeholder="Search Product..."
+                    required=""
+                    type="text"
+                  />
+                  <button type="submit" className="search_btn">
+                    <i className="linearicons-magnifier" />
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+      <BottomHeader />
+    </header>
+  );
+};
+
+export default Header;
---
+++ pages/index.js
@@ -0,0 +1,153 @@
+import React, { useState } from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+import Head from 'next/head'
+
+import Banner from '../components/Banner'
+import FeaturedProductHeader from '../components/FeaturedProductHeader'
+import ProductCard from '../components/ProductCard'
+import BannerSlider from '../components/bannerSliders/BannerSlider'
+
+
+function Home() {
+  
+  const [showTabs,setShowTabs] = useState(true)
+  const [showProductActionBox, setShowProductActionBox] = useState(true);
+
+
+ 
+
+  const products = [
+    {
+      imageUrl: 'assets/images/product_img1.jpg',
+      title: 'Product 1',
+      price: '$20',
+      salePercent: '25%',
+      rating: 4.5,
+      del: 150000,
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    },
+    {
+      imageUrl: 'assets/images/product_img1.jpg',
+      title: 'Product 2',
+      price: '$30',
+      salePercent: '30%',
+      rating: 4.2,
+      description: 'Nullam id varius nunc id varius nunc.',
+    },
+    // Add more products as needed
+    {
+      imageUrl: 'assets/images/product_img1.jpg',
+      title: 'Product 1',
+      price: '$20',
+      salePercent: '25%',
+      rating: 4.5,
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    },
+    {
+      imageUrl: 'assets/images/product_img1.jpg',
+      title: 'Product 2',
+      price: '$30',
+      salePercent: '30%',
+      rating: 4.2,
+      description: 'Nullam id varius nunc id varius nunc.',
+    },
+    // Add more products as needed
+  ];
+
+
+  return (
+    <>
+      {/* <!-- START SECTION BANNER --> */}
+      <div>
+        {/* Example usage of the BannerSlider component */}
+        <BannerSlider  />
+      </div>
+      {/* <!-- END SECTION BANNER --> */}
+      {/* START MAIN CONTENT  */}
+      <>
+        <div className="main_content">
+          {/* START SECTION SHOP */}
+          <div className="section small_pb">
+            <div className="container">
+              <FeaturedProductHeader title="Exclusive Products" showTabs={showTabs} />
+              <div className="row">
+                
+                {products.map((product, index) => (
+                  <div key={index} className="col-md-3">
+                    <ProductCard {...product} showProductActionBox={showProductActionBox} />
+                  </div>
+                ))}
+
+              </div>
+            </div>
+          </div>
+          {/* END SECTION SHOP */}
+          {/* START SECTION BANNER */}
+          <div className="section pb_20 small_pt">
+            <div className="container">
+              <div className="row">
+                <div className="col-md-6">
+                  <Banner
+                    image="assets/images/shop_banner_img1.jpg"
+                    title1="Super Sale"
+                    title2="New Collection"
+                    buttonLink="shop-left-sidebar.html"
+                    buttonText="Shop Now"
+                  />
+                </div>
+                <div className="col-md-6">
+                  <Banner
+                    image="assets/images/shop_banner_img2.jpg"
+                    title1="Sale 40% Off"
+                    title2="New Season"
+                    buttonLink="shop-left-sidebar.html"
+                    buttonText="Shop Now"
+
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* END SECTION BANNER */}
+          {/* START SECTION SHOP */}
+          <div className="section small_pt small_pb">
+            <div className="container">
+              <FeaturedProductHeader title="Deal Of The Days " />
+              <div className="row">
+                {products.map((product, index) => (
+                  <div key={index} className="col-md-3">
+                    <ProductCard {...product}  showProductActionBox={showProductActionBox}  />
+                  </div>
+                ))}
+
+              </div>
+            </div>
+          </div>
+          {/* END SECTION SHOP */}
+
+          {/* START SECTION SHOP */}
+          <div className="section small_pt pb_20">
+            <div className="container">
+              <FeaturedProductHeader title="Here is 3 item in one row " showTabs={showTabs} />
+              <div className="row">
+                {products.map((product, index) => (
+                  <div key={index} className="col-md-4">
+                    {/* <div className='custom-item'>  */}
+                    <ProductCard {...product}  />
+                    {/* </div> */}
+                  </div>
+                ))}
+
+              </div>
+            </div>
+          </div>
+          {/* END SECTION SHOP */}
+        </div>
+      </>
+      {/* END MAIN CONTENT  */}
+    </>
+  )
+}
+
+export default Home
